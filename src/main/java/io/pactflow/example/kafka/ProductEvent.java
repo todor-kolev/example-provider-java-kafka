bd3839e5066a76222b369728948be15f0fd12108
tkolev <todor@devflow.tools>
2023-09-16 17:47:44 +0100

    Rename extraField to optParam

--- src/main/java/io/pactflow/example/kafka/ProductEvent.java
+++ src/main/java/io/pactflow/example/kafka/ProductEvent.java
@@ -15,18 +15,18 @@ class ProductEvent {
   private String version;
   private Double price;
   private EventType event;
-  private String extraField;
+  private String optParam;
 
   ProductEvent() {}
 
 //  ProductEvent(String id, String name, String type, String version, EventType event, Double price) {
-  ProductEvent(String id, String name, String type, String version, EventType event, Double price, String extraField) {
+  ProductEvent(String id, String name, String type, String version, EventType event, Double price, String optParam) {
     this.id = id;
     this.name = name;
     this.type = type;
     this.version = version;
     this.event = event;
     this.price = price;
-    this.extraField = extraField;
+    this.optParam = optParam;
   }
 }
